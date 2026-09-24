b62373ea11c4239a5ca20563d55a923e366ad3b5
DavidB <david.bernard@fms-ea.com>
2026-09-24 09:50:38 +0200

    service correctly created

--- ex3/Angular/meteo-web/src/app/services/forecast.ts
+++ ex3/Angular/meteo-web/src/app/services/forecast.ts
@@ -1,4 +1,9 @@
-export default class ForecastService {
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Forecast {
   async getForecast(lat: number, lon: number) {
     const response = await fetch(`http://localhost:3000/weather?lat=${lat}&lon=${lon}`);
     return await response.json();
